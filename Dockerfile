fce0e00b345692baadcb6e6afa7da47da610a347
Ryan Stortz <ryan@withzombies.com>
2022-05-05 21:47:37 -0400

    .

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ghost:4.47.0-alpine
 
 RUN apk update && apk add curl python3
 
+COPY config.template.json config.template.json
 COPY write-config.py write-config.py
 RUN chmod +x write-config.py
 
--- write-config.py
+++ write-config.py
@@ -3,5 +3,14 @@
 import os
 import json
 
-for key in os.environ:
-    print(key, os.environ[key])
+config = json.load(open("config.template.json"))
+
+deploy_env = os.environ.get("NODE_ENV", "production")
+
+config["server"]["port"] = int(os.environ["PORT"])
+config["database"]["connection"]["url"] = os.environ["JAWSDB_URL"]
+config["paths"]["contentPath"] = os.environ["GHOST_CONTENT"]
+
+print(config)
+
+json.dump(config, open(f'config.{deploy_env}.json', 'w'))
