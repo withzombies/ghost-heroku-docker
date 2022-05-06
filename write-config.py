693f19b4aa207abbbfd736329b07ea5917fb2a61
Ryan Stortz <ryan@withzombies.com>
2022-05-05 22:21:47 -0400

    .

--- write-config.py
+++ write-config.py
@@ -2,13 +2,23 @@
 
 import os
 import json
+from urllib.parse import urlparse
 
 config = json.load(open("config.template.json"))
 
 deploy_env = os.environ.get("NODE_ENV", "production")
 
 config["server"]["port"] = int(os.environ["PORT"])
-config["database"]["connection"] = os.environ["JAWSDB_URL"]
+
+db = urlparse(os.environ["JAWSDB_URL"])
+
+config["database"]["connection"] = {
+    "host": db.hostname,
+    "port": db.port,
+    "user": db.username,
+    "password": db.username,
+    "database": db.path[1:]
+}
 config["paths"]["contentPath"] = os.environ["GHOST_CONTENT"]
 
 print(config)
