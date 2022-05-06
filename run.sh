908212ad9fb4e4eb709bfc8202ed1bd342a26fbd
Ryan Stortz <ryan@withzombies.com>
2022-05-06 18:04:24 -0400

    .

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ set -v
 node create-config.js
 export DEBUG=ghost:*,ghost-config
 cp config.production.json $GHOST_INSTALL/current/config.production.json
-yarn --cwd current/ run knex-migrator init --mgpath $GHOST_INSTALL/current || yarn --cwd current/ run knex-migrator migrate --mgpath $GHOST_INSTALL/current
+yarn --cwd current/ run knex-migrator migrate --mgpath $GHOST_INSTALL/current
 node current/index.js
